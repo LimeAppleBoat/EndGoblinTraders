ba70c1fc68e32c59194bfbb7adde998638ced05f
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-10-17 15:23:54 +1100

    Create TradesMixin.java

---
+++ src/main/java/com/jab125/egt/mixin/TradesMixin.java
@@ -0,0 +1,12 @@
+package com.jab125.egt.mixin;
+
+import net.hat.gt.init.ModTrades;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(ModTrades.class)
+public class TradesMixin {
+    static {
+
+    }
+}
