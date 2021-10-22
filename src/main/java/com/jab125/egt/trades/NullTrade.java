3a77d3b9fb1a4448984c57363035f0ac298103e3
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-10-22 16:38:47 +1100

    Create NullTrade.java

---
+++ src/main/java/com/jab125/egt/trades/NullTrade.java
@@ -0,0 +1,19 @@
+package com.jab125.egt.trades;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradeOffers;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Random;
+
+public class NullTrade implements TradeOffers.Factory {
+    public NullTrade() {
+
+    }
+    @Nullable
+    @Override
+    public TradeOffer create(Entity entity, Random random) {
+        return null;
+    }
+}
