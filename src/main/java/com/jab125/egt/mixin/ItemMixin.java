2ed5ba7623b21f4ab28145a1e708e4636b0c74c2
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-10-22 16:38:45 +1100

    Update ItemMixin.java

--- src/main/java/com/jab125/egt/mixin/ItemMixin.java
+++ src/main/java/com/jab125/egt/mixin/ItemMixin.java
@@ -1,12 +1,14 @@
 package com.jab125.egt.mixin;
 
 import com.jab125.egt.EGobT;
+import com.jab125.egt.init.ModBlocks;
 import com.jab125.egt.init.ModItems;
 import net.hat.gt.GobT;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
@@ -14,6 +16,12 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
 
 import java.util.List;
 
@@ -22,9 +30,14 @@ public abstract class ItemMixin {
     @Shadow
     public abstract String getTranslationKey();
 
-    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-        if (stack.isOf(ModItems.OPAL) && !EGobT.config.GENERATE_OPAL_ORE) {
-            tooltip.add(new TranslatableText(this.getTranslationKey() + ".desc").formatted(Formatting.GRAY));
+
+    @Inject(method = "appendTooltip", at = @At(value = "HEAD"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    public void append(ItemStack stack, World world, List<Text> tooltip, TooltipContext context, CallbackInfo ci) {
+        if (stack.isOf(ModBlocks.OPAL_ORE.asItem()) && !EGobT.config.GENERATE_OPAL_ORE) {
+            tooltip.add(new TranslatableText(this.getTranslationKey() + ".disabled").formatted(Formatting.GRAY));
+        }
+        if(stack.isOf(ModItems.ENCHANTED_GOLDEN_CARROT)) {
+            tooltip.add(new LiteralText("WIP").formatted(Formatting.GRAY));
         }
     }
 }
